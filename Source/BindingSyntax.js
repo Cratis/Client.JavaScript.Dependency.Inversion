2edcb553a548bc417152882807310062da971ff7
Einar Ingebrigtsen <einar@dolittle.com>
2016-03-04 15:51:13 +0100

    Putting in license header

--- Source/BindingSyntax.js
+++ Source/BindingSyntax.js
@@ -1,3 +1,7 @@
+/*---------------------------------------------------------------------------------------------
+ *  Copyright (c) Einar Ingebrigtsen. All rights reserved.
+ *  Licensed under the MIT License. See License.txt in the project root for license information.
+ *--------------------------------------------------------------------------------------------*/
 import {ScopeSyntax} from "./ScopeSyntax";
 import {ConstantActivationStrategy} from "./Strategies/ConstantActivationStrategy";
 import {TypeActivationStrategy} from "./Strategies/TypeActivationStrategy";
--- Source/Strategies/TypeActivationStrategy.js
+++ Source/Strategies/TypeActivationStrategy.js
@@ -1,3 +1,7 @@
+/*---------------------------------------------------------------------------------------------
+ *  Copyright (c) Einar Ingebrigtsen. All rights reserved.
+ *  Licensed under the MIT License. See License.txt in the project root for license information.
+ *--------------------------------------------------------------------------------------------*/
 import {ActivationStrategy} from "./ActivationStrategy";
 
 export class TypeActivationStrategy extends ActivationStrategy
