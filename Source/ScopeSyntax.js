c8532b361ba3ccc62c7fd8ab9337142dba5c257d
Einar Ingebrigtsen <einar@dolittle.com>
2016-03-07 21:42:11 +0100

    JSDoc comments

--- Source/ScopeSyntax.js
+++ Source/ScopeSyntax.js
@@ -2,12 +2,22 @@
  *  Copyright (c) Einar Ingebrigtsen. All rights reserved.
  *  Licensed under the MIT License. See License.txt in the project root for license information.
  *--------------------------------------------------------------------------------------------*/
+import {Scope} from "./Scopes/Scope";
+
 export class ScopeSyntax
 {
+    /**
+     * Define the scope to be a singleton
+     */
     inSingletonScope() {
         
     }
     
+    /**
+     * Define a specific scope as lifecycle
+     * 
+     * @param {Scope} scope The scope representing the lifecycle
+     */
     inScope(scope) {
         
     }
