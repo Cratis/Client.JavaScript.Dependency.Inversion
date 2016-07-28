dbc3260ffc16197260d3c21557a4c943dfa22adf
Einar Ingebrigtsen <einar@dolittle.com>
2016-07-28 16:55:48 -0700

    Up and running - no errors, no failed tests

--- jspm.config.js
+++ jspm.config.js
@@ -2,6 +2,7 @@ SystemJS.config({
     paths: {
         "npm:": "jspm_packages/npm/",
         "github:": "jspm_packages/github/",
+        "local:": "jspm_packages/local/",
         "cratis.client.javascript.dependencyinversion/": "Source/"
     },
     browserConfig: {
@@ -31,15 +32,19 @@ SystemJS.config({
     packageConfigPaths: [
         "npm:@*/*.json",
         "npm:*.json",
-        "github:*/*.json"
+        "github:*/*.json",
+        "local:*.json"
     ],
     map: {
+        "Distribution": "local:Distribution@master",
         "assert": "github:jspm/nodelibs-assert@0.2.0-alpha",
         "buffer": "github:jspm/nodelibs-buffer@0.2.0-alpha",
         "child_process": "github:jspm/nodelibs-child_process@0.2.0-alpha",
         "process": "github:jspm/nodelibs-process@0.2.0-alpha",
         "util": "github:jspm/nodelibs-util@0.2.0-alpha",
-        "vm": "github:jspm/nodelibs-vm@0.2.0-alpha"
+        "vm": "github:jspm/nodelibs-vm@0.2.0-alpha",
+        "cratis.javascript.core": "npm:cratis.javascript.core@1.0.0",
+        "cratis.javascript.reflection": "npm:cratis.javascript.reflection@1.0.0"
     },
     packages: {
         "github:jspm/nodelibs-buffer@0.2.0-alpha": {
