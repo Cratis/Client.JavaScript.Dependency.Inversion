af6301b94c7ffecefd6ea6cef4662c0e68d23376
Einar Ingebrigtsen <einar@dolittle.com>
2016-07-28 16:17:22 -0700

    Changing it all to work with the pipeline

--- jspm.config.js
+++ jspm.config.js
@@ -1,42 +1,28 @@
 SystemJS.config({
-    defaultJSExtensions: true,
     paths: {
+        "npm:": "jspm_packages/npm/",
         "github:": "jspm_packages/github/",
-        "npm:": "jspm_packages/npm/"
+        "cratis.client.javascript.dependencyinversion/": "Source/"
     },
     browserConfig: {
-        "paths": {
-            "cratis.client.javascript.dependencyinversion/": "/src/"
-        }
-    },
-    nodeConfig: {
-        "paths": {
-            "cratis.client.javascript.dependencyinversion/": "src/"
-        }
+        "baseURL": "/"
     },
     devConfig: {
         "map": {
-            "babel-runtime": "npm:babel-runtime@5.8.38",
-            "core-js": "npm:core-js@1.2.7",
-            "fs": "github:jspm/nodelibs-fs@0.2.0-alpha",
-            "path": "github:jspm/nodelibs-path@0.2.0-alpha",
             "plugin-babel": "npm:systemjs-plugin-babel@0.0.12"
         }
     },
     transpiler: "plugin-babel",
-    babelOptions: {
-        "optional": [
-            "runtime",
-            "optimisation.modules.system",
-            "es7.decorators"
-        ]
-    },
-    map: {
-        "babel": "npm:babel-core@5.8.35"
-    },
     packages: {
         "cratis.client.javascript.dependencyinversion": {
-            "main": "cratis.client.javascript.dependencyinversion.js"
+            "main": "index.js",
+            "format": "csj",
+            "defaultExtension": "js",
+            "meta": {
+                "*.js": {
+                    "loader": "plugin-babel"
+                }
+            }
         }
     }
 });
@@ -52,42 +38,10 @@ SystemJS.config({
         "buffer": "github:jspm/nodelibs-buffer@0.2.0-alpha",
         "child_process": "github:jspm/nodelibs-child_process@0.2.0-alpha",
         "process": "github:jspm/nodelibs-process@0.2.0-alpha",
-        "sinon": "npm:sinon@1.17.4",
         "util": "github:jspm/nodelibs-util@0.2.0-alpha",
-        "vm": "github:jspm/nodelibs-vm@0.2.0-alpha",
-        "cratis.client.javascript.core": "npm:cratis.client.javascript.core@1.0.0"
+        "vm": "github:jspm/nodelibs-vm@0.2.0-alpha"
     },
     packages: {
-        "cratis.client.javascript.dependencyinversion": {
-            "main": "index.js",
-            "format": "cjs",
-            "defaultExtension": "js"
-        },
-
-        "npm:formatio@1.1.1": {
-            "map": {
-                "samsam": "npm:samsam@1.1.2"
-            }
-        },
-        "npm:inherits@2.0.1": {
-            "map": {}
-        },
-        "npm:cratis.client.javascript.core@1.0.0": {
-            map: {}
-        },
-        "npm:sinon@1.17.4": {
-            "map": {
-                "formatio": "npm:formatio@1.1.1",
-                "lolex": "npm:lolex@1.3.2",
-                "samsam": "npm:samsam@1.1.2",
-                "util": "npm:util@0.10.3"
-            }
-        },
-        "npm:util@0.10.3": {
-            "map": {
-                "inherits": "npm:inherits@2.0.1"
-            }
-        },
         "github:jspm/nodelibs-buffer@0.2.0-alpha": {
             "map": {
                 "buffer-browserify": "npm:buffer@4.7.1"
@@ -95,9 +49,9 @@ SystemJS.config({
         },
         "npm:buffer@4.7.1": {
             "map": {
-                "ieee754": "npm:ieee754@1.1.6",
+                "base64-js": "npm:base64-js@1.1.2",
                 "isarray": "npm:isarray@1.0.0",
-                "base64-js": "npm:base64-js@1.1.2"
+                "ieee754": "npm:ieee754@1.1.6"
             }
         }
     }
