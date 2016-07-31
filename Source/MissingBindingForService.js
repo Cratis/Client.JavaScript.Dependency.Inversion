cb0d2d8eb4e3e3d531d58599d2a6ff3ab77caaa2
Einar Ingebrigtsen <einar@dolittle.com>
2016-07-31 13:29:12 -0700

    Adding documentation

--- Source/MissingBindingForService.js
+++ Source/MissingBindingForService.js
@@ -4,8 +4,14 @@
  *--------------------------------------------------------------------------------------------*/
 import {Exception} from "cratis-javascript-core";
 
+/**
+ * The exception that gets thrown when no binding for a given service is missing
+ */
 export class MissingBindingForService extends Exception
 {
+    /**
+     * Initializes an instance of {MissingBindingForService}
+     */
     constructor(service) {
         super(`Missing binding for service ´${service}´`);
         this.service = service;
