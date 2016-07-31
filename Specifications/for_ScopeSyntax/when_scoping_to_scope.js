ae3945fa6d05df69df20ea96def93f279d322b65
Einar Ingebrigtsen <einar@dolittle.com>
2016-07-31 07:51:39 -0700

    Scope config up and running

--- Specifications/for_ScopeSyntax/when_scoping_to_scope.js
+++ Specifications/for_ScopeSyntax/when_scoping_to_scope.js
@@ -3,10 +3,11 @@
  *  Licensed under the MIT License. See License.txt in the project root for license information.
  *--------------------------------------------------------------------------------------------*/
 import * as Context from "./given/an_undefined_scope";
-import {SingletonScope} from "../../Source/Scopes/SingletonScope";
+import {Scope} from "../../Source/Scopes/Scope";
 import {Binding} from "../../Source/Binding";
 
-describe("when scoping to singleton", () => {
+describe("when scoping to scope", () => {
+    let scope = new Scope();
     let context = null;
     let binding = null;
 
@@ -17,11 +18,11 @@ describe("when scoping to singleton", () => {
         }; 
 
         (becauseOf => {
-            context.scopeSyntax.asSingleton();
+            context.scopeSyntax.as(scope);
         })();
     });
 
-    it("should set it to be a singleton scope", () => context.scopeSyntax.scope.should.be.instanceof(SingletonScope));
+    it("should set it to be the given scope", () => context.scopeSyntax.scope.should.equal(scope));
     it("should add a binding to the container", () => binding.should.be.instanceof(Binding));
     it("should pass the service to the binding", () => binding.service.should.equal(context.bindingSyntax.service));
     it("should pass the strategy to the binding", () => binding.strategy.should.equal(context.bindingSyntax.strategy));
