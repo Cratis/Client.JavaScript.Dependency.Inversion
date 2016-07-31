6118375e4f49fed69cead08ed9b068093b0a9d15
Einar Ingebrigtsen <einar@dolittle.com>
2016-07-31 12:58:21 -0700

    Dependency extraction up and running

---
+++ Specifications/for_Dependencies/given/a_dependencies_instance.js
@@ -0,0 +1,12 @@
+/*---------------------------------------------------------------------------------------------
+ *  Copyright (c) Einar Ingebrigtsen. All rights reserved.
+ *  Licensed under the MIT License. See License.txt in the project root for license information.
+ *--------------------------------------------------------------------------------------------*/
+import {Dependencies} from "../../../Source/Dependencies";
+
+export default class
+{
+    constructor() {
+        this.dependencies = new Dependencies();
+    }
+}
