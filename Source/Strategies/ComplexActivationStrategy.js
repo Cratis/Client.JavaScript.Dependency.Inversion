fab0fe1f9fb4d67491cc1e29fa95cd71780cf00e
Einar Ingebrigtsen <einar@dolittle.com>
2016-07-31 16:36:32 -0700

    Complex strategy up and running
    However - a failing spec here. Got to figure out how to use Promise in specs. This might be specific to Wallaby - Promises aren't completed, there is a trace error in the console output of the browser.

--- Source/Strategies/ComplexActivationStrategy.js
+++ Source/Strategies/ComplexActivationStrategy.js
@@ -3,8 +3,42 @@
  *  Licensed under the MIT License. See License.txt in the project root for license information.
  *--------------------------------------------------------------------------------------------*/
 import {ActivationStrategy} from "./ActivationStrategy";
+import {Dependencies} from "../Dependencies";
 
-export class ComplexActivationStrategy extends ActivationStrategy
+const _dependencies = new Dependencies();
+
+/**
+ * Represents an activation strategy that is capable of resolving dependencies for a type
+ * and then inject this into the types constructor during creation
+ */
+export class ComplexActivationStrategy extends ActivationStrategy 
 {
-    
+    constructor(container) {
+        super();
+        this.container = container;
+    }
+
+    activate(activationContext, binding) {
+        let dependencies = _dependencies.getFrom(binding.target);
+        let dependencyInstances = [];
+        let container = this.container;
+        let dependencyCount = 0;
+
+        var promise = new Promise((resolve, reject) => {
+            dependencies.forEach((dependency, dependencyIndex) => {
+                container.get(dependency).then(instance => {
+                    dependencyInstances[dependencyIndex] = instance;
+                    dependencyCount++;
+
+                    if( dependencyCount == dependencies.length ) {
+                        let args = [null].concat(dependencyInstances);
+                        var type = binding.target.bind.apply(binding.target, args);
+                        var typeInstance = new type();
+                        resolve(typeInstance);
+                    }
+                });
+            });
+        });
+        return promise;
+    }
 }
